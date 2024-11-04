60a9d689745eeabc56f64ce2010842cd0fb0bae8
adrian <117434901+AdrianProje@users.noreply.github.com>
2024-11-04 09:40:43 +0100

    Update 1.1App.jsx

--- .github/workflows/1.1App.jsx
+++ .github/workflows/1.1App.jsx
@@ -25,22 +25,6 @@ const App = () => {
     
       // <Content ... />
       // <Total ... />
-
-
-      
-    // <div>
-    //   <h1>{course}</h1>
-    //   <p>
-    //     {part1} {exercises1}
-    //   </p>
-    //   <p>
-    //     {part2} {exercises2}
-    //   </p>
-    //   <p>
-    //     {part3} {exercises3}
-    //   </p>
-    //   <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    // </div>
       
   )
 }
