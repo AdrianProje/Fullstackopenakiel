e2192e3e08f3d69c64b9daa775f4f7de5fc71b6c
adrian <117434901+AdrianProje@users.noreply.github.com>
2025-06-05 14:29:33 +0200

    Update server.js

--- part2/server.js
+++ part2/server.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import path from 'path';
 
-const path = path();
-const app = express();
-
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Przechwyć wszystkie pozostałe żądania i oddaj index.html
