1693d5693f9e52e1033da775a6267c700edb5cac
adrian <117434901+AdrianProje@users.noreply.github.com>
2025-06-13 10:32:21 +0200

    Update server.js

--- part2/server.js
+++ part2/server.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url'; // Służy do konwersji URL modułu na ścieżkę pliku
+
+// Najpierw przekształcamy import.meta.url na __filename...
+const __filename = fileURLToPath(import.meta.url);
+// ...następnie wyznaczamy katalog, w którym znajduje się ten plik
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -7,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 // Przechwyć wszystkie pozostałe żądania i oddaj index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'App'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
